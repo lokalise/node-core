7add678d721741aaf1490e752216847b21a5bab5
Igor Savin <iselwin@gmail.com>
2022-08-28 12:53:31 +0300

    Base repository structure and http client

---
+++ index.ts
@@ -0,0 +1,14 @@
+export {
+  sendPut,
+  sendDelete,
+  sendPatch,
+  sendGet,
+  sendPost,
+  httpClient,
+} from './src/http/httpClient'
+export type {
+  GetRequestOptions,
+  DeleteRequestOptions,
+  RequestOptions,
+  Response,
+} from './src/http/httpClient'
---
+++ src/http/httpClient.ts
@@ -0,0 +1,183 @@
+import type { Dispatcher } from 'undici'
+import { request } from 'undici'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type RecordObject = Record<string, any>
+
+export type GetRequestOptions = {
+  headers?: RecordObject
+  query?: RecordObject
+  timeout?: number
+  throwOnError?: boolean
+}
+
+export type DeleteRequestOptions = GetRequestOptions
+
+export type RequestOptions = {
+  headers?: RecordObject
+  query?: RecordObject
+  timeout?: number
+  throwOnError?: boolean
+}
+
+const defaultOptions: GetRequestOptions = {
+  throwOnError: true,
+  timeout: 30000,
+}
+
+export type Response<T> = {
+  body: T
+  headers: RecordObject
+  statusCode: number
+}
+
+export async function sendGet<T>(
+  baseUrl: string,
+  path: string,
+  options: GetRequestOptions = defaultOptions,
+): Promise<Response<T>> {
+  const url = resolveUrl(baseUrl, path)
+  const response = await request(url, {
+    method: 'GET',
+    query: options.query,
+    headers: options.headers,
+    bodyTimeout: options.timeout,
+    throwOnError: options.throwOnError,
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const resolvedBody = await resolveBody(response)
+
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    body: resolvedBody,
+    headers: response.headers,
+    statusCode: response.statusCode,
+  }
+}
+
+export async function sendDelete<T>(
+  baseUrl: string,
+  path: string,
+  options: DeleteRequestOptions = defaultOptions,
+): Promise<Response<T>> {
+  const url = resolveUrl(baseUrl, path)
+  const response = await request(url, {
+    method: 'DELETE',
+    query: options.query,
+    headers: options.headers,
+    bodyTimeout: options.timeout,
+    throwOnError: options.throwOnError,
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const resolvedBody = await resolveBody(response)
+
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    body: resolvedBody,
+    headers: response.headers,
+    statusCode: response.statusCode,
+  }
+}
+
+export async function sendPost<T>(
+  baseUrl: string,
+  path: string,
+  body?: RecordObject | undefined,
+  options: RequestOptions = defaultOptions,
+): Promise<Response<T>> {
+  const url = resolveUrl(baseUrl, path)
+  const response = await request(url, {
+    method: 'POST',
+    body: body ? JSON.stringify(body) : undefined,
+    query: options.query,
+    headers: options.headers,
+    bodyTimeout: options.timeout,
+    throwOnError: options.throwOnError,
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const resolvedBody = await resolveBody(response)
+
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    body: resolvedBody,
+    headers: response.headers,
+    statusCode: response.statusCode,
+  }
+}
+
+export async function sendPut<T>(
+  baseUrl: string,
+  path: string,
+  body?: RecordObject | undefined,
+  options: RequestOptions = defaultOptions,
+): Promise<Response<T>> {
+  const url = resolveUrl(baseUrl, path)
+  const response = await request(url, {
+    method: 'PUT',
+    body: body ? JSON.stringify(body) : undefined,
+    query: options.query,
+    headers: options.headers,
+    bodyTimeout: options.timeout,
+    throwOnError: options.throwOnError,
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const resolvedBody = await resolveBody(response)
+
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    body: resolvedBody,
+    headers: response.headers,
+    statusCode: response.statusCode,
+  }
+}
+
+export async function sendPatch<T>(
+  baseUrl: string,
+  path: string,
+  body?: RecordObject | undefined,
+  options: RequestOptions = defaultOptions,
+): Promise<Response<T>> {
+  const url = resolveUrl(baseUrl, path)
+  const response = await request(url, {
+    method: 'PATCH',
+    body: body ? JSON.stringify(body) : undefined,
+    query: options.query,
+    headers: options.headers,
+    bodyTimeout: options.timeout,
+    throwOnError: options.throwOnError,
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const resolvedBody = await resolveBody(response)
+
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    body: resolvedBody,
+    headers: response.headers,
+    statusCode: response.statusCode,
+  }
+}
+
+async function resolveBody(response: Dispatcher.ResponseData) {
+  const contentType = response.headers['content-type']
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return contentType?.startsWith('application/json')
+    ? await response.body.json()
+    : await response.body.text()
+}
+
+function resolveUrl(baseUrl: string, path: string): string {
+  return new URL(path, baseUrl).href
+}
+
+export const httpClient = {
+  get: sendGet,
+  post: sendPost,
+  put: sendPut,
+  patch: sendPatch,
+  del: sendDelete,
+}
