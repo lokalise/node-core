6bf575cf6ed41dbd9e136869f13ebd7c6fc61fe5
Igor Savin <iselwin@gmail.com>
2023-06-06 19:15:29 +0300

    Switch to vitest (#51)

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    watch: false,
+    environment: 'node',
+    reporters: ['verbose'],
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts', 'src/config/configTypes.ts', 'src/errors/either.ts'],
+      reporter: ['text'],
+      all: true,
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
+    },
+  },
+})
