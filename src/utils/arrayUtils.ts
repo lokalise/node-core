9760ed30f91ac7182c32145b73de1cc67698f4fb
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2023-10-17 19:42:20 +0200

    Improving removeFalsy return type
    Co-authored-by: Igor Savin <iselwin@gmail.com>

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -36,6 +36,6 @@ export function removeNullish<const T>(array: readonly (T | null | undefined)[])
 /**
  * Return a copy of the given array without falsy values (eg: false, 0, '', null, undefined)
  */
-export function removeFalsy<const T>(array: readonly (T | null | undefined)[]): T[] {
+export function removeFalsy<const T>(array: readonly (T | null | undefined | 0 | '' | false)[]): T[] {
   return array.filter((e) => e) as T[]
 }
