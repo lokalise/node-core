875205f8a2978dc6c3a570ce47c973d3493e1ddf
CarlosGamero <carlos.gamero@lokalise.com>
2024-01-23 12:10:22 +0100

    Adding minor comments

--- src/utils/objectUtils.ts
+++ src/utils/objectUtils.ts
@@ -204,10 +204,10 @@ export function transformToKebabCase<Output, Input>(
 
   const transformKey = (key: string) =>
     key
-      .replace(/([a-z])([A-Z])/g, '$1-$2')
-      .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
-      .replace(/_/g, '-')
-      .toLowerCase()
+      .replace(/([a-z])([A-Z])/g, '$1-$2') // transforms basic camelCase
+      .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2') // transforms abbreviations
+      .replace(/_/g, '-') // transforms snake_case
+      .toLowerCase() // finally lowercase all
 
   return Object.entries(object as Record<string, unknown>).reduce(
     (result, [key, value]) => ({
