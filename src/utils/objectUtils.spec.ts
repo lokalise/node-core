1f1af6da9749f2b942124557327cbb95216df6d9
Anton Malich <anton.malich@lokalise.com>
2024-02-22 18:01:32 +0300

    * add group by path function

--- src/utils/objectUtils.spec.ts
+++ src/utils/objectUtils.spec.ts
@@ -3,6 +3,7 @@ import {
   copyWithoutUndefined,
   deepClone,
   groupBy,
+  groupByPath,
   groupByUnique,
   isEmptyObject,
   pick,
@@ -330,6 +331,246 @@ describe('objectUtils', () => {
       ]
 
       const result = groupBy(input, 'id')
+
+      expect(result).toStrictEqual({
+        1: [
+          {
+            id: 1,
+            name: 'a',
+            bool: true,
+          },
+          {
+            id: 1,
+            name: 'b',
+            bool: true,
+          },
+        ],
+      })
+    })
+  })
+
+  describe('groupByPath', () => {
+    it('Empty array', () => {
+      const array: { id: { nestedId: string } }[] = []
+      const result = groupByPath(array, 'id.nestedId')
+      expect(Object.keys(result)).length(0)
+    })
+
+    type TestType = {
+      id?: number | null
+      name: string
+      bool: boolean
+      nested?: {
+        code: number
+      }
+    }
+
+    it('Correctly groups by string values', () => {
+      const input: TestType[] = [
+        {
+          id: 1,
+          name: 'a',
+          bool: true,
+          nested: { code: 100 },
+        },
+        {
+          id: 2,
+          name: 'c',
+          bool: true,
+          nested: { code: 200 },
+        },
+        {
+          id: 3,
+          name: 'b',
+          bool: true,
+          nested: { code: 300 },
+        },
+        {
+          id: 4,
+          name: 'a',
+          bool: true,
+          nested: { code: 400 },
+        },
+      ]
+
+      const result: Record<string, TestType[]> = groupByPath(input, 'name')
+      expect(result).toStrictEqual({
+        a: [
+          {
+            id: 1,
+            name: 'a',
+            bool: true,
+            nested: { code: 100 },
+          },
+          {
+            id: 4,
+            name: 'a',
+            bool: true,
+            nested: { code: 400 },
+          },
+        ],
+        b: [
+          {
+            id: 3,
+            name: 'b',
+            bool: true,
+            nested: { code: 300 },
+          },
+        ],
+        c: [
+          {
+            id: 2,
+            name: 'c',
+            bool: true,
+            nested: { code: 200 },
+          },
+        ],
+      })
+    })
+
+    it('Correctly groups by nested string values', () => {
+      const input: TestType[] = [
+        {
+          id: 1,
+          name: 'a',
+          bool: true,
+          nested: { code: 100 },
+        },
+        {
+          id: 2,
+          name: 'c',
+          bool: true,
+          nested: { code: 200 },
+        },
+        {
+          id: 3,
+          name: 'b',
+          bool: true,
+          nested: { code: 300 },
+        },
+        {
+          id: 4,
+          name: 'a',
+          bool: true,
+          nested: { code: 100 },
+        },
+      ]
+
+      const result: Record<string, TestType[]> = groupByPath(input, 'nested.code')
+      expect(result).toStrictEqual({
+        100: [
+          {
+            id: 1,
+            name: 'a',
+            bool: true,
+            nested: { code: 100 },
+          },
+          {
+            id: 4,
+            name: 'a',
+            bool: true,
+            nested: { code: 100 },
+          },
+        ],
+        300: [
+          {
+            id: 3,
+            name: 'b',
+            bool: true,
+            nested: { code: 300 },
+          },
+        ],
+        200: [
+          {
+            id: 2,
+            name: 'c',
+            bool: true,
+            nested: { code: 200 },
+          },
+        ],
+      })
+    })
+
+    it('Correctly groups by number values', () => {
+      const input: TestType[] = [
+        {
+          id: 1,
+          name: 'a',
+          bool: true,
+        },
+        {
+          id: 1,
+          name: 'b',
+          bool: false,
+        },
+        {
+          id: 2,
+          name: 'c',
+          bool: false,
+        },
+        {
+          id: 3,
+          name: 'd',
+          bool: false,
+        },
+      ]
+
+      const result: Record<number, TestType[]> = groupByPath(input, 'id')
+
+      expect(result).toStrictEqual({
+        1: [
+          {
+            id: 1,
+            name: 'a',
+            bool: true,
+          },
+          {
+            id: 1,
+            name: 'b',
+            bool: false,
+          },
+        ],
+        2: [
+          {
+            id: 2,
+            name: 'c',
+            bool: false,
+          },
+        ],
+        3: [
+          {
+            id: 3,
+            name: 'd',
+            bool: false,
+          },
+        ],
+      })
+    })
+
+    it('Correctly handles undefined and null', () => {
+      const input: TestType[] = [
+        {
+          id: 1,
+          name: 'a',
+          bool: true,
+        },
+        {
+          name: 'c',
+          bool: true,
+        },
+        {
+          id: null,
+          name: 'd',
+          bool: true,
+        },
+        {
+          id: 1,
+          name: 'b',
+          bool: true,
+        },
+      ]
+
+      const result = groupByPath(input, 'id')
 
       expect(result).toStrictEqual({
         1: [
