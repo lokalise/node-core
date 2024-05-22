b0aa7789c35bb9b212e852ff249d304725dca8b9
Julio Montoya <julio.montoya@lokalise.com>
2024-05-22 12:24:36 +0200

    AP-3631 Add hash utils

---
+++ src/utils/hashUtils.ts
@@ -0,0 +1,15 @@
+import { createHash } from 'crypto'
+
+export enum HashDigest {
+  HEX = 'hex',
+  BASE64 = 'base64',
+}
+
+export enum HashAlgorithm {
+  SHA256 = 'sha256',
+  SHA512 = 'sha512',
+}
+
+export function generateHash(data: string, algorythm: HashAlgorithm, digest: HashDigest) {
+  return createHash(algorythm).update(data).digest(digest)
+}
