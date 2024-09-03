671eea8641648e17b2833646bd9cef1755aa7c66
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2024-09-03 16:56:27 +0200

    AP-5078 MultiTransactionObservabilityManager (#158)
    * AP-5078 MultiTransactionObservabilityManager
    
    * Lint fix
    
    * AP-5078 Removing empty check
    
    * Adding doc
    
    * Typo fix

---
+++ src/observability/MultiTransactionObservabilityManager.ts
@@ -0,0 +1,35 @@
+import type { TransactionObservabilityManager } from './observabilityTypes'
+
+/**
+ * Groups different TransactionObservabilityManager instances into one
+ * to facilitate tracking transactions across multiple observability tools.
+ */
+export class MultiTransactionObservabilityManager implements TransactionObservabilityManager {
+  private readonly managers: TransactionObservabilityManager[]
+
+  constructor(managers: TransactionObservabilityManager[]) {
+    this.managers = managers
+  }
+
+  start(transactionName: string, uniqueTransactionKey: string): void {
+    for (const manager of this.managers) {
+      manager.start(transactionName, uniqueTransactionKey)
+    }
+  }
+
+  startWithGroup(
+    transactionName: string,
+    uniqueTransactionKey: string,
+    transactionGroup: string,
+  ): void {
+    for (const manager of this.managers) {
+      manager.startWithGroup(transactionName, uniqueTransactionKey, transactionGroup)
+    }
+  }
+
+  stop(uniqueTransactionKey: string, wasSuccessful?: boolean): void {
+    for (const manager of this.managers) {
+      manager.stop(uniqueTransactionKey, wasSuccessful)
+    }
+  }
+}
