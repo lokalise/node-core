31f3b8f81e839b2f0f276cddbbab75e206bec291
Mateusz Tkacz <mateusz.tkacz@lokalise.com>
2025-05-15 10:35:19 +0200

    chore: remove redundant check for error index

--- src/errors/InstanceofSafeError.ts
+++ src/errors/InstanceofSafeError.ts
@@ -25,10 +25,6 @@ const getPrototypeNamesPostError = (input: unknown): string[] => {
 
   const errorIndex = reversedNames.indexOf(Error.name)
 
-  if (errorIndex === -1) {
-    return reversedNames
-  }
-
   return reversedNames.slice(errorIndex + 1)
 }
 
